65d9d98c7700f3a66fad8fb7b40e87256c2827ea
Priti Wright <priti@recurrentauto.com>
2022-03-23 15:17:07 -0700

    Update src/VehiclesTable.tsx

--- src/VehiclesTable.tsx
+++ src/VehiclesTable.tsx
@@ -34,9 +34,6 @@ export default function VehiclesTable() {
         setData(data);
         setLoading(false);
       })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
   }, []);
 
   return (
