bd1ca5a0316489ff29e38e9aecc877531a383497
Priti Wright <priti@recurrentauto.com>
2022-03-23 15:01:35 -0700

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -6,5 +6,3 @@
 import "@testing-library/jest-dom/extend-expect";
 
 require("jest-fetch-mock").enableMocks();
-// changes default behavior of fetchMock to use the real 'fetch' implementation and not mock responses
-// fetchMock.dontMock();
